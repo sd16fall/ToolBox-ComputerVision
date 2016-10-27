55ba593534d4caacf161b421c1225d92a3759e4d
lqyeric94 <lqyeric94@gmail.com>
2016-10-27 02:15:27 -0400

    turning in my tool box 2

--- face_detect.py
+++ face_detect.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-img = cv2.imread("img.jpg")
+img = cv2.imread("img1.jpg")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 faces=face_cascade.detectMultiScale(gray,1.3,5)
 
